b2fb43bafb62f39dfb823f20d82025cce4f57212
Ragesh <ragesh@kiviclouds.com>
2023-08-13 21:03:25 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,166 @@
+// swiper    
+var mySwiper = new Swiper('.swiper-container', {
+    effect: '',
+    loop: true,
+    speed: 1000,
+    slidesPerView: 1,
+    navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+       el: '.swiper-pagination',
+       type: 'bullets',
+       clickable: 'true'
+    },
+    autoplay: {
+      delay: 5000,
+    },
+ });
+
+//  Multi slider
+
+$(".custom-carousel").owlCarousel({
+   autoWidth: true,
+   loop: false,
+   items : 1,
+   // nav    : true,
+ });
+ $(document).ready(function () {
+   $(".custom-carousel .item").click(function () {
+     $(".custom-carousel .item").not($(this)).removeClass("active");
+     $(this).toggleClass("active");
+   });
+ });
+ 
+
+//  Testimonials
+
+// vars
+'use strict'
+var	testim = document.getElementById("testim"),
+		testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+		touchStartPos,
+		touchEndPos,
+		touchPosDiff,
+		ignoreTouch = 30;
+;
+
+window.onload = function() {
+
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length-1;
+        }
+
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");            
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+    
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function() {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+
+    testimLeftArrow.addEventListener("click", function() {
+        playSlide(currentSlide -= 1);
+    })
+
+    testimRightArrow.addEventListener("click", function() {
+        playSlide(currentSlide += 1);
+    })    
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function() {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+
+    playSlide(currentSlide);
+
+    // keyboard shortcuts
+    document.addEventListener("keyup", function(e) {
+        switch (e.keyCode) {
+            case 37:
+                testimLeftArrow.click();
+                break;
+                
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            default:
+                break;
+        }
+    })
+		
+		testim.addEventListener("touchstart", function(e) {
+				touchStartPos = e.changedTouches[0].clientX;
+		})
+	
+		testim.addEventListener("touchend", function(e) {
+				touchEndPos = e.changedTouches[0].clientX;
+			
+				touchPosDiff = touchStartPos - touchEndPos;
+			
+				console.log(touchPosDiff);
+				console.log(touchStartPos);	
+				console.log(touchEndPos);	
+
+			
+				if (touchPosDiff > 0 + ignoreTouch) {
+						testimLeftArrow.click();
+				} else if (touchPosDiff < 0 - ignoreTouch) {
+						testimRightArrow.click();
+				} else {
+					return;
+				}
+			
+		})
+}
+
+
+$(function() {
+
+    $('.info').first().show().animate({
+      width: '60%'
+    });
+    $('.item').click(function() {
+      $(this).next().show().animate({
+        width: '60%'
+      }).siblings('.info').animate({
+        width: '0%'
+      }, function() {
+        $(this).hide();
+      });
+      $(this).addClass('test').siblings().removeClass('test');
+    });
+  });
+       
